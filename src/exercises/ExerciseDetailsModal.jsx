857b1e52fd14d8c165cf7a205b4e5930f939d23c
johnsona128-coder <johnsona128@vcu.edu>
2025-11-16 19:41:20 -0500

    Backing out of the photo slider and went with static photo

--- src/exercises/ExerciseDetailsModal.jsx
+++ src/exercises/ExerciseDetailsModal.jsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 //Getting a reusable connection for fetching data and one central location for setting the URL for the API
 import fetchData, { apiURL as helperApiURL } from "../components/utils";
+import AutoImageSlider from "../components/photos";
 
 const baseApiURL = helperApiURL.replace(/\/$/, "");
 
@@ -56,51 +57,67 @@ export default function ExerciseDetailsModal({ show, onClose, exerciseId }) {
 
   if (!show) return null;
 
+
+
   return (
-    <div className="ModalOutside" onClick={onClose}>
-      <div className="ModalInside">
-        {loading && <p>Loading...</p>}
-        {error && <p style={{ color: "red" }}>{error}</p>}
+    <div className="ModalOutside">
 
-        {exercise && (
-          <>
-            <h2>{exercise.exerciseName}</h2>
-            <p>{exercise.category} - {exercise.equipment}</p>
 
-            <h3>Instructions</h3>
-            {steps.length > 0 ? (
-              <ol>
-                {steps.map((s) => (
-                  <li key={s.id}>{s.instruction}</li>
-                ))}
-              </ol>
-            ) : (
-              <p>No instructions available.</p>
-            )}
-            <div className="exercise-grid">
-              <div className="item">
-                <span className="detailsTitle">Force</span><br />{exercise.forceType || "N/A"}
-              </div>
-              <div className="item">
-                <span className="detailsTitle">Level</span><br />{exercise.experienceLevel || "N/A"}
-              </div>
-              <div className="item">
-                <span className="detailsTitle">Mechanic</span><br />{exercise.mechanic || "N/A"}
-              </div>
-              <div className="item">
-                <span className="detailsTitle">Primary</span><br />{exercise.primaryMuscles || "N/A"}
-              </div>
-              <div className="item">
-                <span className="detailsTitle">Secondary</span><br />{exercise.secondaryMuscles || "N/A"}
-              </div>
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {exercise && (
+        <>
+          <h2>{exercise.exerciseName}</h2>
+          <p>{exercise.category} - {exercise.equipment}</p>
+
+          <h3>Instructions</h3>
+          {steps.length > 0 ? (
+            <ol>
+              {steps.map((s) => (
+                <li key={s.id}>{s.instruction}</li>
+              ))}
+            </ol>
+          ) : (
+            <p>No instructions available.</p>
+          )}
+
+          <div className="exercise-grid">
+            <div className="item">
+              <span className="detailsTitle">Force</span><br />{exercise.forceType || "N/A"}
+            </div>
+            <div className="item">
+              <span className="detailsTitle">Level</span><br />{exercise.experienceLevel || "N/A"}
+            </div>
+            <div className="item">
+              <span className="detailsTitle">Mechanic</span><br />{exercise.mechanic || "N/A"}
+            </div>
+            <div className="item">
+              <span className="detailsTitle">Primary</span><br />{exercise.primaryMuscles || "N/A"}
+            </div>
+            <div className="item">
+              <span className="detailsTitle">Secondary</span><br />{exercise.secondaryMuscles || "N/A"}
+            </div>
+          </div>
+
+          {photos.length > 0 ? (
+            <div className="photos" >
+
+              <img src={`${baseApiURL}/${photos[0].imagePath}`} className="exercisePhoto" />
+
             </div>
 
+          ) : (
+            <p>No photos are available.</p>
+          )}
 
+          <div>
             <button className='submitBtn' onClick={onClose}>Close</button>
+          </div>
+        </>
+      )
+      }
+    </div >
 
-          </>
-        )}
-      </div>
-    </div>
   );
 }
