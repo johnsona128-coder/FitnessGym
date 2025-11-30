8b531b2a94690368eabbd50887655e8ba803c470
johnsona128-coder <johnsona128@vcu.edu>
2025-11-30 16:55:08 -0500

    Attemping to correct errors introduced by merges.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,7 @@
-
-import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import App from './App';
 
 const container = document.getElementById("app");
-const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
